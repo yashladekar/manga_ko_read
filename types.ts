7dbc95f070b9c03627da50febfd5267ac17be340
yashladekar <yashladekar002@gmail.com>
2024-08-12 12:30:24 +0530

    feat: Add new interfaces and types for manga website
    :-  made changes to types and added types according to the manga with there providers responses

--- types.ts
+++ types.ts
@@ -64,4 +64,307 @@ export type sources =
 
 export interface Subscription {
   endpoint: string;
+  keys: {
+    p256dh: string;
+    auth: string;
+  };
 }
+
+export interface SelectType {
+  vlaue: string;
+  lable: string;
+}
+
+export interface ImgRatio {
+  index: number;
+  ratio: number;
+}
+
+export type ImageMode =
+  | "contain"
+  | "cover"
+  | "fill"
+  | "fitW"
+  | "fitH"
+  | "scale-down"
+  | "none";
+
+export type ReadMode = "vertical" | "horizontal" | "webtoon" | "continuous";
+
+export type ReadDirection = "ltr" | "rtl";
+
+export type NavigateDirection = "next" | "prev";
+
+export type NextDirection = "next" | "prev" | "first" | "last";
+
+export interface ComicFollowed {
+  _id: string;
+  mangaSlug: string;
+  userId: string;
+  createAt: Date;
+  source: string;
+  status: string;
+}
+
+export type FollowState =
+  | "reading"
+  | "on-hold"
+  | "dropped"
+  | "plan-to-read"
+  | "completed"
+  | "re-reading"
+  | "author";
+
+export interface ReadModeSettings {
+  readMode: ReadMode;
+  readDirection: ReadDirection;
+  nextDirection: NextDirection;
+  autoNext?: boolean;
+}
+
+export type SourcesId = "NTC" | "LHM" | "OTK" | "MNG" | "MGR";
+
+export interface MangaResource {
+  sourceName: string;
+  sourceId: SourcesId;
+}
+
+export type Status = "In progress" | "Completed" | "On hold" | "Dropped";
+
+export type NETTRUYEN_GENRES =
+  | "action"
+  | "truong-thanh"
+  | "adventure"
+  | "anime"
+  | "chuyen-sinh-213"
+  | "comedy-99"
+  | "comic"
+  | "cooking"
+  | "co-dai-207"
+  | "doujinshi"
+  | "drama-103"
+  | "dam-my"
+  | "ecchi"
+  | "fantasy-105"
+  | "gender-bender"
+  | "harem-107"
+  | "historical"
+  | "horror"
+  | "josei"
+  | "live-action"
+  | "manga-112"
+  | "manhua"
+  | "manhwa-11400"
+  | "martial-arts"
+  | "mature"
+  | "mecha-117"
+  | "mystery"
+  | "ngon-tinh"
+  | "one-shot"
+  | "psychological"
+  | "romance-121"
+  | "school-life"
+  | "sci-fi"
+  | "seinen"
+  | "shoujo"
+  | "shoujo-ai-126"
+  | "shounen-127"
+  | "shounen-ai"
+  | "slice-of-life"
+  | "smut"
+  | "soft-yaoi"
+  | "soft-yuri"
+  | "sports"
+  | "supernatural"
+  | "tap-chi-truyen-tranh"
+  | "thieu-nhi"
+  | "tragedy-136"
+  | "trinh-tham"
+  | "truyen-scan"
+  | "truyen-mau"
+  | "viet-nam"
+  | "webtoon"
+  | "xuyen-khong-205"
+  | "16";
+
+// gnenres need to be updated to match with the new genres
+
+export interface ServerResponse {
+  success: boolean;
+  totalPage: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+}
+
+export interface NtSearchResponseData {
+  thumbnail: string;
+  name: string;
+  slug: string;
+  newChapter: string;
+  genres: string[];
+}
+
+export interface Manga extends NtSearchResponseData {
+  status: Status;
+  author: string;
+  otherName: string;
+  review: string;
+  updatedAt: string;
+  chapters?: Chapter[];
+}
+export interface MangaDetails {
+  title: string;
+  updatedAt: string;
+  otherName: string;
+  author: string;
+  thumbnail: string;
+  status: string;
+  genres: Genre[];
+  view: string;
+  review: string;
+  chapterList: Chapter[];
+}
+export type HeadlessManga = Pick<MangaDetails, "chapterList" | "title"> & {
+  mangaSlug: string;
+  isWebtoon?: boolean;
+};
+export interface Chapter {
+  chapterId: string;
+  chapterNumber: string;
+  chapterTitle: string;
+  chapterSlug?: string;
+  updatedAt: string;
+  view: string;
+  _id?: string;
+}
+
+export interface Genre {
+  genreTitle: string;
+  slug: string;
+}
+
+export interface LHSearchRes {
+  success: boolean;
+  data: DataLHSearchRes;
+}
+
+export interface DataLHSearchRes {
+  status: number;
+  success: boolean;
+  data: LHMangaSearch[];
+}
+
+export interface LHMangaSearch {
+  id: number;
+  name: string;
+  cover_url: string;
+  pilot: string;
+  url: string;
+}
+
+export interface ImagesChapter {
+  id: string;
+  imgSrc: string;
+  imgSrcCDN?: string;
+}
+
+export interface Page_Image {
+  id: string;
+  src: string;
+  fallbackSrc?: string;
+}
+
+export interface Page extends Page_Image {
+  _id: string;
+}
+
+export interface ChapterDetails {
+  _id: string;
+  comicName: string;
+  comicSlug: string;
+  source: string;
+  chapters_list: { _id: string; sourceName: string; chapters: Chapter[] }[];
+}
+
+export interface PageInfo {
+  _id: string;
+  chapterSlug: string;
+  comicName: string;
+  comicSlug: string;
+  pages: Page[];
+  source: string;
+}
+
+export interface Comic {
+  _id: string;
+  name: string;
+  __v: number;
+  author: string;
+  custom_id: number;
+  genres: Genre[];
+  chapters?: ChapterDetails; // -> fallback comic hasn't chapters
+  description?: Description; // -> fallback comic hasn't description
+  newChapter: string;
+  otherName: string;
+  review: string;
+  slug: string;
+  sourcesAvailable: SourcesAvailable[];
+  status: string;
+  thumbnail: string;
+  updatedAt: string;
+  votes?: string[];
+}
+
+export interface Genre {
+  id: string;
+  value: string;
+  label: string;
+  _id: string;
+}
+
+export interface Character {
+  _id: string;
+  cover: string;
+  mal_url: string;
+  name: string;
+  role: string;
+}
+
+export interface DescPicture {
+  large: string;
+  small: string;
+  _id: string;
+}
+
+export interface Description {
+  _id: string;
+  name: string;
+  characters: Character[];
+  titles: {
+    title_synonyms: string;
+    title_japanese: string;
+    title_english: string;
+  };
+  cover: string;
+  description: string;
+  mal_id: string;
+  popularity: string;
+  published: string;
+  ranked: string;
+  score: string;
+  slug: string;
+  pictures: DescPicture[];
+}
+
+export interface SourcesAvailable {
+  sourceName: string;
+  sourceSlug: string;
+  _id: string;
+}
+
+export type ViewSelection =
+  | "Chapters"
+  | "Characters"
+  | "Details"
+  | "Pictures"
+  | "Recommendations";
